4575861f5f762fe06e492ff6e543abed0034f195
KOJIMEISTER <nikitun_r@mail.ru>
2024-09-05 18:01:19 +0300

    linux

--- WavAnalyzer.h
+++ WavAnalyzer.h
@@ -27,22 +27,22 @@ public:
 
     struct WavHeader
     {
-        char chunkId[4];
-        unsigned long chunkSize;
-        char format[4];
-        char subchunk1Id[4];
-        unsigned long subchunk1Size;
-        unsigned short audioFormat;
-        unsigned short numChannels;
-        unsigned long sampleRate;
+        char chunkId[4]{0,0,0,0};
+        __uint32_t chunkSize=0;
+        char format[4]{0,0,0,0};
+        char subchunk1Id[4]{0,0,0,0};
+        __uint32_t subchunk1Size = 0;
+        __uint16_t audioFormat = 0;
+        __uint16_t numChannels = 0;
+        __uint32_t sampleRate = 0;
         // Кол-во байт, переданных за секунду воспроизведения
-        unsigned long byteRate;
+        __uint32_t byteRate = 0;
         // Количество байт для блока из сэмплов аудиодорожек
-        unsigned short blockAlign;
-        unsigned short bitsPerSample;
-        char subchunk2Id[4];
+        __uint16_t blockAlign = 0;
+        __uint16_t bitsPerSample = 0;
+        char subchunk2Id[4]{0,0,0,0};
         // Всего байт, выделенных под звук
-        unsigned long subchunk2Size;
+        __uint32_t subchunk2Size = 0;
     };
 
 	AnalyzeRes Analyze();
